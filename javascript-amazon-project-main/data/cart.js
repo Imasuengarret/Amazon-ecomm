d0ff7f04745d42613d59dbd320a319ff1828d5e5
imasuengarret <garretharry@gmail.com>
2025-04-13 01:30:40 +0100

    loading cart from local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,40 +1,54 @@
-export let cart = [
+export let cart = JSON.parse(
+  localStorage.getItem("cart")
+); /* JSON.parse to convert to an array*/
+if (!cart) {
+  cart = [
     {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    Quantity: 2     
-},{
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    Quantity: 1
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      Quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      Quantity: 1,
+    },
+  ];
 }
-];
 
- export const addToCart = (productId) =>{
-    let matchingItem;
+const saveToStorage = () => {
+  localStorage.setItem(
+    "cart",
+    JSON.stringify(cart)
+  ); /* JSON.stringify converts to string becos local storage can only save strings*/
+};
 
-    cart.forEach((cartItem) =>{
-        if(productId === cartItem.productId){
-            matchingItem = cartItem;
-        }
-    })
-    if(matchingItem){
-        matchingItem.Quantity += 1;
-    } else{
-        cart.push({
-            productId: productId,
-            Quantity: 1
-        })
+export const addToCart = (productId) => {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
+  });
+  if (matchingItem) {
+    matchingItem.Quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      Quantity: 1,
+    });
+  }
+  saveToStorage();
+};
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+
+  saveToStorage();
 }
-
-
-export function removeFromCart(productId){
-    const newCart = [];
-
-    cart.forEach((cartItem) =>{
-        if(cartItem.productId !== productId){
-            newCart.push(cartItem)
-        }
-    })
-    cart = newCart;
-}
-
