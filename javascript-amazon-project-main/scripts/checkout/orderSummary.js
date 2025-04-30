912b4bee82dcdd8245bef5d5c02a1bdff823797b
imasuengarret <garretharry@gmail.com>
2025-04-30 22:33:45 +0100

    createautomated for orderSummary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -28,7 +28,7 @@ cart.forEach((cartItem) => {
   const dateString = deliveryDate.format("dddd, MMMM D");
 
   cartSummaryHtml += `
-    <div class="cart-item-container js-cart-item-container-${
+    <div class="cart-item-container js-cart-item-container js-cart-item-container-${
       matchingProduct.id
     }">
       <div class="delivery-date">
@@ -44,14 +44,14 @@ cart.forEach((cartItem) => {
           <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label">${cartItem.Quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link"
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}"
               data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -133,4 +133,4 @@ document.querySelectorAll('.js-delivery-option').forEach((element) =>{
 })
 }
 
-renderOrderSummary();
+// renderOrderSummary();
